ff978e94ab4aecf29ee665bb75a1f2d89aaed618
Portocan <cportocan@gmail.com>
2022-01-08 14:05:14 -0700

    fixed number check on names and created Job class

--- main.py
+++ main.py
@@ -1,42 +1,71 @@
 class Name:
 
     def __init__(self):
-        print("Welcome to my character creator!")
         self.firstname = ""
         self.lastname = ""
         self.charname = ""
         self.badname = True
 
     def playername(self):
+        first = ""
+        last = ""
         print("First thing is first, I'm going to have you enter your name.")
-        first = input("What's your first name? ")
 
-        last = input("What's your last name? ")
+        while self.badname:
+            first = input("What's your first name? ")
+            self.checkname(first)
 
+        # resetting badname check
+        self.badname = True
+
+        while self.badname:
+            last = input("What's your last name? ")
+            self.checkname(last)
+
+        self.badname = True     #resetting badname check
+
+        #formatting player name
         self.firstname = first.title()
         self.lastname = last.title()
         return self.lastname, self.firstname
 
     def avname(self):
-        charname = input("Now, please enter your character's name. ")
+        charname = ""
 
-        for letter in charname:
+        while self.badname:
+            charname = input("Now, please enter your character's name. ")
+            self.checkname(charname)
+
+        self.badname = True
+        #formatting  character name
+        self.charname = charname.title()
+        return self.charname
+
+    #Used to check names for numbers and flag them if that is the case
+    def checkname(self, name):
+        for letter in name:
             if letter.isdigit():
                 self.badname = True
+                print("You can't enter numbers.")
             else:
                 self.badname = False
             return self.badname
 
-        self.charname = charname.title()
-        return self.charname
+
+class Job:
+
+    def __init__(self):
+        self.charjob = ""
 
 
-class Character(Name):
-    player = Name()
+class Character(Name, Job):
+    Name()
+    Job()
 
 
 class Play:
 
+    print("Welcome to my character creator!")
     #player name creation
     player = Character()
     player.playername()
@@ -44,11 +73,7 @@ class Play:
 
     #character name creation
     player.avname()
-
-    if player.badname:
-        print("Don't enter numbers, you aren't a robot")
-        player.avname()
-    print(player.charname)
+    print(player.charname + " is a fine name!")
 
 
 if __name__ == "__main__":
